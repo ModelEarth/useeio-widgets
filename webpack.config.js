55d0ad049c43c9a4fce1481818dcd7e11530430d
Foris8 <foristang@gmail.com>
2024-04-29 21:41:15 -0500

    leetcode

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
 const path = require('path');
 const buildDir = path.resolve(__dirname, 'build');
 const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack'); // Import webpack to access built-in plugins
 
 const config = {
     entry: {
-        // we could also generate a small JS library for every widget
-        // 'ImpactChart': './src/impact-chart.ts',
-        // 'FilterWidget': './src/filter-widget.ts',
-        // 'SectorDelete': './src/sector-list.ts',
         'useeio_widgets': './src/main.ts',
     },
     module: {
@@ -26,49 +23,42 @@ const config = {
         filename: '[name].js',
         path: buildDir + '/lib',
         libraryTarget: 'var',
-        library: ['useeio' /*, '[name]' */],
+        library: ['useeio'],
     },
-
     plugins: [
         new CopyPlugin({
             patterns: [
-                { from: 'src/**/*.html', to:path.resolve(__dirname, 'build','','[name][ext]')},
-                { from: 'src/**/*.css', to: path.resolve(__dirname, 'build','','[name][ext]')},
+                { from: 'src/**/*.html', to: path.resolve(__dirname, 'build', '', '[name][ext]') },
+                { from: 'src/**/*.css', to: path.resolve(__dirname, 'build', '', '[name][ext]') },
                 {
                     from: 'node_modules/apexcharts/dist/apexcharts.min.js',
-                    to: buildDir + '/lib/apexcharts.min.js', 
+                    to: buildDir + '/lib/apexcharts.min.js',
                     toType: 'file'
                 },
                 {
                     from: 'node_modules/apexcharts/dist/apexcharts.css',
-                    to: buildDir + '/lib/apexcharts.css', 
+                    to: buildDir + '/lib/apexcharts.css',
                     toType: 'file'
                 },
-                /*
-                // We compile React into the generated library but we could
-                // also reference it as an external dependency (see below). 
-                // This would be useful when React is already available in
-                // the page where the widgets whould be included.
-                {
-                    from: 'node_modules/react/umd/react.production.min.js',
-                    to: buildDir + '/lib/react.production.min.js', toType: 'file'
-                },
-                {
-                    from: 'node_modules/react-dom/umd/react-dom.production.min.js',
-                    to: buildDir + '/lib/react-dom.production.min.js', toType: 'file'
-                },
-                */
             ]
         }),
+        // Enable HMR globally
+        new webpack.HotModuleReplacementPlugin(),
     ],
-
     externals: {
         "apexcharts": "apexcharts",
-        /*
-        "react": "React",
-        "react-dom": "ReactDOM",
-        */
-    }
+    },
+    // Configure devServer
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'build'),
+        },
+        hot: false,
+        liveReload: true,
+        open: true,
+        port: 3000, // or any port of your choice
+        },
+
 };
 
 module.exports = (_env, argv) => {
